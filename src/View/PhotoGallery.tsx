31a0d498f05efb65f269cc5dcd8c8b7592f510fb
Emilixn <emilien.muck@gmail.com>
2023-04-29 00:17:46 +0200

    Gallery and font

--- src/View/PhotoGallery.tsx
+++ src/View/PhotoGallery.tsx
@@ -1,3 +1,5 @@
+import { SlMagnifierAdd } from 'react-icons/all';
+
 import Footer from '../Component/Footer';
 import Menu from '../Component/Menu';
 
@@ -9,38 +11,136 @@ const PhotoGallery = () => {
       <div className="grid grid-cols-3  w-[80%] m-auto   mt-10 gap-10">
         <div className="relative overflow-hidden bg-no-repeat bg-cover  group">
           <img className="object-cover   w-full h-60 rounded-2xl " src="../src/img/cropped-1920-1080-872716.jpg" />
-          <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 group-hover:opacity-40 transition duration-700 ease-in-out bg-white  rounded-2xl"></div>
-          <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full  overflow-hidden text-white opacity-0 group-hover:opacity-100 transition duration-700 ease-in-out  rounded-2xl"></div>
-        </div>
+          <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 group-hover:opacity-70 transition duration-700 ease-in-out bg-white  rounded-2xl"></div>
+          <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full  overflow-hidden text-white opacity-0 group-hover:opacity-100 transition duration-700 ease-in-out  rounded-2xl">
+            <div className="text-black flex justify-center h-full items-center">
+              <div className="justify-between">
+                <div className="flex justify-center">
+                  <button>
+                    <div className="p-4 rounded-full bg-primary">
+                      <SlMagnifierAdd color="white" size={30}></SlMagnifierAdd>
+                    </div>
+                  </button>
+                </div>
+                <br />
+                <div className="flex text-2xl">Chantier la walck</div>
+              </div>
+            </div>
+          </div>
+        </div>{' '}
         <div className="relative overflow-hidden bg-no-repeat bg-cover  group">
           <img className="object-cover   w-full h-60 rounded-2xl " src="../src/img/cropped-1920-1080-872716.jpg" />
-          <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 group-hover:opacity-40 transition duration-700 ease-in-out bg-white  rounded-2xl"></div>
-          <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full  overflow-hidden text-white opacity-0 group-hover:opacity-100 transition duration-700 ease-in-out  rounded-2xl"></div>
-        </div>
+          <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 group-hover:opacity-70 transition duration-700 ease-in-out bg-white  rounded-2xl"></div>
+          <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full  overflow-hidden text-white opacity-0 group-hover:opacity-100 transition duration-700 ease-in-out  rounded-2xl">
+            <div className="text-black flex justify-center h-full items-center">
+              <div className="justify-between">
+                <div className="flex justify-center">
+                  <button>
+                    <div className="p-4 rounded-full bg-primary">
+                      <SlMagnifierAdd color="white" size={30}></SlMagnifierAdd>
+                    </div>
+                  </button>
+                </div>
+                <br />
+                <div className="flex text-2xl">Chantier la walck</div>
+              </div>
+            </div>
+          </div>
+        </div>{' '}
         <div className="relative overflow-hidden bg-no-repeat bg-cover  group">
           <img className="object-cover   w-full h-60 rounded-2xl " src="../src/img/cropped-1920-1080-872716.jpg" />
-          <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 group-hover:opacity-40 transition duration-700 ease-in-out bg-white  rounded-2xl"></div>
-          <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full  overflow-hidden text-white opacity-0 group-hover:opacity-100 transition duration-700 ease-in-out  rounded-2xl"></div>
-        </div>
+          <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 group-hover:opacity-70 transition duration-700 ease-in-out bg-white  rounded-2xl"></div>
+          <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full  overflow-hidden text-white opacity-0 group-hover:opacity-100 transition duration-700 ease-in-out  rounded-2xl">
+            <div className="text-black flex justify-center h-full items-center">
+              <div className="justify-between">
+                <div className="flex justify-center">
+                  <button>
+                    <div className="p-4 rounded-full bg-primary">
+                      <SlMagnifierAdd color="white" size={30}></SlMagnifierAdd>
+                    </div>
+                  </button>
+                </div>
+                <br />
+                <div className="flex text-2xl">Chantier la walck</div>
+              </div>
+            </div>
+          </div>
+        </div>{' '}
         <div className="relative overflow-hidden bg-no-repeat bg-cover  group">
           <img className="object-cover   w-full h-60 rounded-2xl " src="../src/img/cropped-1920-1080-872716.jpg" />
-          <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 group-hover:opacity-40 transition duration-700 ease-in-out bg-white  rounded-2xl"></div>
-          <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full  overflow-hidden text-white opacity-0 group-hover:opacity-100 transition duration-700 ease-in-out  rounded-2xl"></div>
-        </div>
+          <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 group-hover:opacity-70 transition duration-700 ease-in-out bg-white  rounded-2xl"></div>
+          <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full  overflow-hidden text-white opacity-0 group-hover:opacity-100 transition duration-700 ease-in-out  rounded-2xl">
+            <div className="text-black flex justify-center h-full items-center">
+              <div className="justify-between">
+                <div className="flex justify-center">
+                  <button>
+                    <div className="p-4 rounded-full bg-primary">
+                      <SlMagnifierAdd color="white" size={30}></SlMagnifierAdd>
+                    </div>
+                  </button>
+                </div>
+                <br />
+                <div className="flex text-2xl">Chantier la walck</div>
+              </div>
+            </div>
+          </div>
+        </div>{' '}
         <div className="relative overflow-hidden bg-no-repeat bg-cover  group">
           <img className="object-cover   w-full h-60 rounded-2xl " src="../src/img/cropped-1920-1080-872716.jpg" />
-          <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 group-hover:opacity-40 transition duration-700 ease-in-out bg-white  rounded-2xl"></div>
-          <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full  overflow-hidden text-white opacity-0 group-hover:opacity-100 transition duration-700 ease-in-out  rounded-2xl"></div>
-        </div>
+          <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 group-hover:opacity-70 transition duration-700 ease-in-out bg-white  rounded-2xl"></div>
+          <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full  overflow-hidden text-white opacity-0 group-hover:opacity-100 transition duration-700 ease-in-out  rounded-2xl">
+            <div className="text-black flex justify-center h-full items-center">
+              <div className="justify-between">
+                <div className="flex justify-center">
+                  <button>
+                    <div className="p-4 rounded-full bg-primary">
+                      <SlMagnifierAdd color="white" size={30}></SlMagnifierAdd>
+                    </div>
+                  </button>
+                </div>
+                <br />
+                <div className="flex text-2xl">Chantier la walck</div>
+              </div>
+            </div>
+          </div>
+        </div>{' '}
         <div className="relative overflow-hidden bg-no-repeat bg-cover  group">
           <img className="object-cover   w-full h-60 rounded-2xl " src="../src/img/cropped-1920-1080-872716.jpg" />
-          <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 group-hover:opacity-40 transition duration-700 ease-in-out bg-white  rounded-2xl"></div>
-          <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full  overflow-hidden text-white opacity-0 group-hover:opacity-100 transition duration-700 ease-in-out  rounded-2xl"></div>
-        </div>
+          <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 group-hover:opacity-70 transition duration-700 ease-in-out bg-white  rounded-2xl"></div>
+          <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full  overflow-hidden text-white opacity-0 group-hover:opacity-100 transition duration-700 ease-in-out  rounded-2xl">
+            <div className="text-black flex justify-center h-full items-center">
+              <div className="justify-between">
+                <div className="flex justify-center">
+                  <button>
+                    <div className="p-4 rounded-full bg-primary">
+                      <SlMagnifierAdd color="white" size={30}></SlMagnifierAdd>
+                    </div>
+                  </button>
+                </div>
+                <br />
+                <div className="flex text-2xl">Chantier la walck</div>
+              </div>
+            </div>
+          </div>
+        </div>{' '}
         <div className="relative overflow-hidden bg-no-repeat bg-cover  group">
           <img className="object-cover   w-full h-60 rounded-2xl " src="../src/img/cropped-1920-1080-872716.jpg" />
-          <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 group-hover:opacity-40 transition duration-700 ease-in-out bg-white  rounded-2xl"></div>
-          <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full  overflow-hidden text-white opacity-0 group-hover:opacity-100 transition duration-700 ease-in-out  rounded-2xl"></div>
+          <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 group-hover:opacity-70 transition duration-700 ease-in-out bg-white  rounded-2xl"></div>
+          <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full  overflow-hidden text-white opacity-0 group-hover:opacity-100 transition duration-700 ease-in-out  rounded-2xl">
+            <div className="text-black flex justify-center h-full items-center">
+              <div className="justify-between">
+                <div className="flex justify-center">
+                  <button>
+                    <div className="p-4 rounded-full bg-primary">
+                      <SlMagnifierAdd color="white" size={30}></SlMagnifierAdd>
+                    </div>
+                  </button>
+                </div>
+                <br />
+                <div className="flex text-2xl">Chantier la walck</div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       Gallery
